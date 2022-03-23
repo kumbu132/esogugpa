10a50e13a06984f5d7865fb25aaaf885c8d7df0b
Kumbukani Kamanga <kkamanga16@gmail.com>
2022-03-23 17:20:53 +0300

    [GPA-14] - add ga and improve seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
-import { ModulesProvider } from '../context/context';
-import Layout from '../components/layout';
-import '../styles/global.css';
+import { useEffect } from "react"
+import { useRouter } from "next/router"
+import * as ga from "../lib/ga"
+import { ModulesProvider } from "../context/context"
+import Layout from "../components/layout"
+import "../styles/global.css"
 export default function App({ Component, pageProps }) {
-	return (
-		<ModulesProvider>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</ModulesProvider>
-	);
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
+  return (
+    <ModulesProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ModulesProvider>
+  )
 }
